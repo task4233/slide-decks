af92cb874dd0fe31aadc0afc2a06c0613ccdc344
task4233 <tks.m1205@gmail.com>
2021-04-12 21:36:35 +0900

    change: コメントを厳密な表現に変更

--- examples/column/empty_interface.go
+++ examples/column/empty_interface.go
@@ -5,10 +5,10 @@ func main() {
 
 	var Num interface{} = -1
 
-	// Numと1の型が異なるのでinvalidな式の評価になる
+	// Numと1の型が異なるので、invalidな式の演算が原因でcompile errorになる
 	// var _ int = Num + 1
 
-	// intとNumのUnderlying typeが異なるのでCoversionできない
+	// intとNumのUnderlying typeが異なるので、Coversionできないことが原因でcompile errorになる
 	// var _ int = int(Num) + 1
 
 	// type assertionすればOK
