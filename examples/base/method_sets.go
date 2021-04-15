74dd239789b2bdf5a890e13a924ad626574ba811
task4233 <tks.m1205@gmail.com>
2021-04-15 17:28:53 +0900

    不必要な部分を削除

--- examples/base/method_sets.go
+++ examples/base/method_sets.go
@@ -1,7 +1,7 @@
 type Num int
 
 func (num Num) A1()        { num++ }
-func (num Num) B1(val int) { num += val }
+func (num Num) B1(val Num) { num += val }
 
 func (num *Num) A2()        { *num++ }
-func (num *Num) B2(val int) { *num += val }
+func (num *Num) B2(val Num) { *num += val }
